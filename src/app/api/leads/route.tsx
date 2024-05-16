88666a9c7e3acacf9331d110fb2a38a28e1b98f3
Dillon McCloud <dillon@hyvv.io>
2024-05-15 17:08:34 -0700

    added GET, POST, PATCH and DELETE api routes for /api/leads

---
+++ src/app/api/leads/route.tsx
@@ -0,0 +1,71 @@
+/* eslint-disable */
+import { db } from "@/server/db";
+import { leads } from "@/server/db/schema";
+import type { NextApiRequest, NextApiResponse } from "next";
+import { InferInsertModel, eq } from "drizzle-orm";
+
+export async function GET(req: NextApiRequest) {
+  const url = new URL(req.url!);
+  const searchParams = new URLSearchParams(url.searchParams);
+
+  if (searchParams.get("getAll")) {
+    const results = await db.query.leads.findMany();
+    return Response.json(results);
+  }
+
+  if (searchParams.get("getByLeadId")) {
+    const id = searchParams.get("getByLeadId");
+    if (!id) {
+      return Response.error();
+    }
+    const result = await db
+      .select()
+      .from(leads)
+      .where(eq(leads.id, parseInt(id)));
+    return Response.json(result);
+  }
+
+  if (searchParams.get("getByOwnerId")) {
+    const ownerId = searchParams.get("getByOwnerId");
+    if (!ownerId) {
+      return Response.error();
+    }
+    const result = await db
+      .select()
+      .from(leads)
+      .where(eq(leads.ownerId, ownerId));
+    return Response.json(result);
+  }
+}
+
+export async function POST(req: Request) {
+  const data: InferInsertModel<typeof leads> = await req.json();
+  const result = await db.insert(leads).values({
+    ...data,
+  });
+  return Response.json(result);
+}
+
+export async function PATCH(req: Request) {
+  const { searchParams } = new URL(req.url!);
+  const id = searchParams.get("id");
+  if (!id) {
+    return Response.error();
+  }
+  const data: InferInsertModel<typeof leads> = await req.json();
+  const result = await db
+    .update(leads)
+    .set(data)
+    .where(eq(leads.id, parseInt(id)));
+  return Response.json(result);
+}
+
+export async function DELETE(req: Request) {
+  const { searchParams } = new URL(req.url!);
+  const id = searchParams.get("id");
+  if (!id) {
+    return Response.error();
+  }
+  const result = await db.delete(leads).where(eq(leads.id, parseInt(id)));
+  return Response.json(result);
+}
